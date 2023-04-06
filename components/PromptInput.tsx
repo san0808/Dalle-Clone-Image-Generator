97971edf0d6d49b3f63a7167f3237d4f4115b83e
Sanket <sanketbhat882002@gmail.com>
2023-04-06 11:48:10 +0530

    setting up states and working on Generate button

--- components/PromptInput.tsx
+++ components/PromptInput.tsx
@@ -1,12 +1,27 @@
+"use client"; 
 import React from 'react'
 
 function PromptInput() {
+    const [prompt, setPrompt] = React.useState('')
+  
+
   return (
     <div className='m-10'>
         <form className='flex flex-col lg:flex-row shadow-md shadow-slate-400/10 border rounded-md lg:divide-x'>
-            <textarea placeholder='Enter a Prompt ...' className='flex-1 p-4 outline-none rounded-md'/>
+            <textarea 
+                value={prompt}
+                onChange={(e) => setPrompt(e.target.value)}
+                placeholder='Enter a Prompt ...' 
+                className='flex-1 p-4 outline-none rounded-md'
+            />
 
-            <button className='' type='submit'>Generate</button>
+            <button 
+                className={`p-4 bg-violet-500 text-white transition-colors duration-200 font-bold rounded-t-md md:rounded-t-none md:rounded-r-md disabled:text-gray-300 disabled:cursor-not-allowed disabled:bg-gray-400`} 
+                type='submit'
+                disabled={prompt.length === 0}
+            >
+                Generate
+            </button>
 
             <button className='p-4 bg-violet-400 text-white transition-colors duration-200 font-bold disabled:text-gray-300 disabled:cursor-not-allowed disabled:bg-gray-400 ' type='button'>
                 Use Suggestion
